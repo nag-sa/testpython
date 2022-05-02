edd257ad431207ed904691755b9dd3f902120b86
nag-sa <52416142+nag-sa@users.noreply.github.com>
2022-05-02 22:12:26 +0530

    Update set.py

--- set.py
+++ set.py
@@ -12,7 +12,37 @@ set1 = {"a", "b", "c", "a", 1,2,3,}
 print(set1)
 
 print(type(set1))
+
 #Add an item to a set, using the add() method
 set1.add("z")
 print(set1)
+
 #To add items from another set into the current set, use the update() method.
+set1 = {"a", "b", "c", "a", 1,2,3,}
+set2= {"x", "y", "z", 4,5,6}
+set1.update(set2)
+print(set1)
+
+#To remove an item in a set, use the remove(), or the discard() method.
+s1 = {"abc","xyz", "lmn"}
+s1.remove("xyz") # error raised when element doesn't exist.
+s1.discard("eshita") # no error raised, when element doesn't exist
+print(s1)
+
+#last element remove using pop method
+s1.pop()
+print(s1)
+
+#to clear set
+#s1.clear()
+#print(s1)#it will return empty set
+
+#The union() method returns a new set with all items from both sets:
+s3 = set1.union(set2)
+print(s3)
+
+#Return a set that contains the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
