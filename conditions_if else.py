df03bc4ac1f00f7eabdaca0f3b36f6dc113352c7
nag-sa <52416142+nag-sa@users.noreply.github.com>
2022-05-02 17:15:34 +0530

    Create conditions_if else.py

---
+++ conditions_if else.py
@@ -0,0 +1,16 @@
+# a = input("please enter a:")
+# b = input("please enter b:") 
+
+# if a > b:
+#     print("a is greater then b")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("b is greater then a")
+
+for x in range(5, 20, 2):
+    print(x)
+for i in range(10):
+    print(i)
+else:
+    print("finished")
