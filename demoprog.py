8ac46e93521c2d8c29835e01fc18c8d05645ec75
nag-sa <52416142+nag-sa@users.noreply.github.com>
2022-05-02 22:40:39 +0530

    Update demoprog.py

--- demoprog.py
+++ demoprog.py
@@ -19,3 +19,18 @@ for key , value in dict.items():
     #print(key, value)
     if "o" in value:
         print(key)
+        
+#Copy all the groups having value with lettre "o" in a new set called oSet. 
+# Copy remaining groups into a list called "notOList"
+# Print the elements of oSet and notOList.
+oSet=set()
+notOList = []
+dict={"group1": "orange", "group2": "red", "group3": "yellow"}
+for key , value in dict.items():
+    #print(key, value)
+    if "o" in value:
+        oSet.add(key)
+    else:
+        notOList.append(key)
+print("OSet: ", oSet)
+print("notOList: ", notOList)
